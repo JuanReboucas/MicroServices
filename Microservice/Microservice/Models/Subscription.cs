58266d2ec7a4eb78bbd5ad7589cb5bf29571f3b4
Juan Almeida <juan.almeida@hp.com>
2022-09-28 16:01:05 -0300

    feat(0006):foi realizado a criacao da entidade

--- Microservice/Microservice/Models/Subscription.cs
+++ Microservice/Microservice/Models/Subscription.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
         public int StatusId { get; set; }
+
+        public User User { get; set; }
         public Status Status { get; set; }
     }
 }

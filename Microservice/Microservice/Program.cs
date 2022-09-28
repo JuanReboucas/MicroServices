47664cddd675d4d7f783b0f91a32c719d6bef7d1
Juan Almeida <juan.almeida@hp.com>
2022-09-28 13:46:06 -0300

    feat(0001):hello world

---
+++ Microservice/Microservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

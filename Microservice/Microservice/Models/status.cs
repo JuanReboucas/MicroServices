3b2e9d8931e6c7119abac6522b2770299a1f24f4
Juan Almeida <juan.almeida@hp.com>
2022-09-28 14:46:35 -0300

    feat(0004): criacao feita do modulo de status

---
+++ Microservice/Microservice/Models/status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservice.Models
+{
+    public class status
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
